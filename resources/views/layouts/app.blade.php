f3622e50fc2f1ab21994d7b5f2aaf73e598b4294
Federico Frascà <70528215+federicofrasca796@users.noreply.github.com>
2022-01-04 23:16:15 +0100

    add app layout file

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laracast @yield('page-title', 'a')</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400&display=swap" rel="stylesheet">
+        <!-- CSS -->
+        <link rel="stylesheet" href="{{ asset('css/app.css')}}">
+    </head>
+    <body>
+        
+    </body>
+</html>
