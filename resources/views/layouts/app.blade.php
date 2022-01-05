7960059a7fd060eb58265414e8ad8d7a92ca4fbf
Federico Frascà <70528215+federicofrasca796@users.noreply.github.com>
2022-01-05 23:00:19 +0100

    start navbar in siteheader

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laracast @yield('page-title', '')</title>
+        <title>Laracasts @yield('page-title', '')</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.googleapis.com">
